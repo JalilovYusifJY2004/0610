6bd3c7ed60dc2d00ca6bb95270aa28b9e434f13d
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-10-05 21:55:01 +0400

    add

---
+++ Program0610.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace task0610
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            ////int[]n = { 1, 2, 3, 4, 5, 6, };
+
+            ////İnsertArray (n);
+            ////Console.WriteLine(n);
+            //task2
+            //int balance = 500;
+            //int mebleg = 100;
+            //int cashin = 400;
+            //Console.WriteLine("balance:" + Balance(balance));
+        }
+        //task1
+        //public static void İnsertArray(params int[] numbers)
+        //{
+        //    int[] arr=new int[numbers.Length];
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        arr[i] = numbers[i];
+        //    }
+        //    Console.WriteLine( arr);
+        //}
+        //task2
+        //public static int Balance(int a)
+        //{
+        //    return a;
+        //}
+        //public static int Mebleg(int a,int b)
+        //{ return a - b; }
+        //public static int CashIn(int a, int b)
+        //{ return a + b; }
+
+    }
+}
